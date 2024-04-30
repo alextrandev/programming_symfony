c957eac4b5deef00d4c109490007881ba565f5e6
dk10119 <tranduclong092@gmail.com>
2024-04-30 11:03:54 +0300

    Feat: copy a crud template, create a template and render with controller

--- Symfony_MAMP/web/src/Controller/CRUDListController.php
+++ Symfony_MAMP/web/src/Controller/CRUDListController.php
@@ -3,17 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class CRUDListController extends AbstractController
-{
-    #[Route('/c/r/u/d/list', name: 'app_c_r_u_d_list')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CRUDListController.php',
-        ]);
+class CRUDListController extends AbstractController {
+    #[Route('crud', name: 'crud')]
+    public function index(): Response {
+        return $this->render('crudlist/index.html.twig');
     }
 }
