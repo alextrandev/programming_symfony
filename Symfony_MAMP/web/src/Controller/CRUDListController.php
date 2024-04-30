e5eaeca89443b72b6fcfe432fbcb332afb72fb4d
dk10119 <tranduclong092@gmail.com>
2024-04-30 12:11:39 +0300

    Refactor: move base html body to a new block

--- Symfony_MAMP/web/src/Controller/CRUDListController.php
+++ Symfony_MAMP/web/src/Controller/CRUDListController.php
@@ -7,8 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CRUDListController extends AbstractController {
-    #[Route('crud', name: 'crud')]
+    #[Route('/crud', name: 'app_crud_list')]
     public function index(): Response {
         return $this->render('crudlist/index.html.twig');
+    }
+
+    #[Route('/create', name: 'app_create')]
+    public function create(): Response {
+        exit("To do: create a new task");
     }
 }
